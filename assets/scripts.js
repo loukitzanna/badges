ce3f555849838050c8d01d3cd13d7c3290245c6b
loukitzanna <loukianova@email.arizona.edu>
2015-02-11 14:52:08 -0700

    added main to script

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,9 @@
+var main = function(){
 
 $('#fake-button').click(function(){
 
     $('.toast').fadeIn(400).delay(3000).fadeOut(400); //fade out after 3 seconds
 })
-
+};
+$(document).ready(main);
 
